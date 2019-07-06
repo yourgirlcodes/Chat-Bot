503248c36fa5a9de3f0278d683d5e0826be0dd8b
Zoe Cohen <zoecohen01@gmail.com>
2019-07-06 22:20:45 +0300

    final commit

--- boto.py
+++ boto.py
@@ -1,17 +1,20 @@
 """
 This is the template server side for ChatBot
 """
-from bottle import route, run, template, static_file, request
+from bottle import route, run, template, static_file, request, response
 import json
 import random
 
 
 list_swear = ["fuck", "shit", "motherfucker", "bitch", "slut", "whore", "idiot"]
 list_pronouns = ["you", "your", "yours", "you are", "you're", "youre", "u"]
-list_requests = ["how", "why", "when", "who", "what", "where", "can", "help", "how?", "why?", "when?", "who?", "what?", "where?", "can?", "help?"]
+list_requests = ["how", "why", "when", "who", "what", "where", "can", "help", "how?", "why?", "when?", "who?", "what?",
+                 "where?", "can?", "help?"]
 list_questions = ["?", "??", "???", "??????"]
 animation_default = ["bored", "giggling", "confused", "laughing", "afraid", "dog", "heartbroke", "money", "no", "takeoff"]
-bot_resp_default = ["I don't think we are getting eachother", "Sounds interesting, maybe you could be more clear?", "Great to have you here. I can do alll sorts of things for you! Ask me for a joke, tell me about yourself "]
+bot_resp_default = ["I don't think we are getting eachother", "Sounds interesting, maybe you could be more clear?",
+                    "Great to have you here. I can do alll sorts of things for you! "
+                    "Ask me for a joke, tell me about yourself "]
 list_request_joke = ["tell", "joke", "jokes", "something", "funny"]
 jokes_archive = ["Today at the bank, an old lady asked me to help check her balance. So I pushed her over.",
                  "I bought some shoes from a drug dealer. "
@@ -22,6 +25,11 @@ jokes_archive = ["Today at the bank, an old lady asked me to help check her bala
                  "She still isn't talking to me.", "Whatdya call a frenchman wearing sandals? "
                                                    "Phillipe Phillope."]
 general_words = ["thanks", "hello", "name"]
+dog_fact = ["Dogs and cats both slurp water the same way.",
+            "Your dog’s sense of smell is 1,000 to 10 million times better than yours.",
+            "Dogs can hear 4 times as far as humans.",
+            "Your dog can smell your feelings."]
+dog_words = ["dogs", "dogs?", "doggy", "puppy", "dog?", "dog"]
 
 
 @route('/', method='GET')
@@ -29,11 +37,34 @@ def index():
     return template("chatbot.html")
 
 
+@route('/hello')
+def hello_again():
+    if request.get_cookie("visited"):
+        return "Hey, you're back!"
+    else:
+        response.set_cookie("visited", "yes")
+
+
 @route("/chat", method='POST')
 def chat():
-    user_message = request.POST.get('msg')
+    user_message = request.POST.get('msg').lower()
+    if request.get_cookie("visited"):
+        feedback = conversation(user_message)
+        return feedback
+    else:
+        response.set_cookie("visited", "yes")
+        feedback = handle_first_question(user_message)
+        return feedback
+
+
+def conversation(user_message):
     bot_output = parsing(user_message)
     return json.dumps(bot_output)
+
+
+def handle_first_question(user_message):
+    bot_response = ("Hey %s!" % user_message)
+    return json.dumps({"animation": "giggling", "msg": bot_response})
 
 
 def parsing(user_message):
@@ -45,21 +76,21 @@ def parsing(user_message):
         return {"animation": animation, "msg": bot_response}
 
     if any(word in list_pronouns for word in user_split_message):
-        bot_response = "This should not be about me, let's talk about you. I am here to help you... How can I help you"
+        bot_response = "You seem to really like me."
         animation = "excited"
         return {"animation": animation, "msg": bot_response}
 
-    if any(word in list_requests for word in user_split_message):
-        question = classify_request(user_message)
+    if '?' in user_message:
+        question = classify_request(user_split_message)
         return question
 
     if any(word in list_request_joke for word in user_split_message):
         animation = animation_default[randomize(5)]
-        bot_response = jokes_archive[randomize(5)]
+        bot_response = jokes_archive[randomize(4)]
         return {"animation": animation, "msg": bot_response}
 
     if any(word in general_words for word in user_split_message):
-        animation : animation_default[randomize(9)]
+        animation = animation_default[randomize(8)]
         bot_response = bot_resp_default[2]
         return {"animation": animation, "msg": bot_response}
 
@@ -73,13 +104,16 @@ def randomize(num):
         return rand
 
 
-def classify_request(user_message):
-    if any(i in list_questions for i in user_message):
-        bot_response = "I'll have to get back to you on that."
-        animation = "ok"
-        return {"animation": animation, "msg": bot_response}
+def classify_request(user_split_message):
+    if any(word in dog_words for word in user_split_message):
+        return {"animation": "dog", "msg": dog_fact[randomize(3)]}
     else:
-        return {"animation": animation_default[1], "msg": bot_resp_default[1]}
+        bot_response = 'If you are trying to ask me a question, ' \
+                       'I only know how to tell you about dogs. so type \'dogs\' ' \
+                       'along with what you would like to know... ' \
+                       'eg: \'dogs ? -  and I will tell you a random dog fact.'
+        animation = "dog"
+        return {"animation": animation, "msg": bot_response}
 
 
 @route("/test", method='POST')
